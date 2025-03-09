ddd8069a4acabf96b50a8942841dc85ad5227468
SamuelOchieng <o.samuelyannick@gmail.com>
2025-03-09 09:13:27 +0300

    Commit

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 54, "M", "Brown", "Cool", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 20, "M", "Brown", "Aggressive", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha" , 18, "F", "White", "Cool", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 40, "M", "Brown", "Cool", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa" , 22, "F", "Brown", "Cool", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch" , 24, "M", "Brown", "Cool", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy" , 29, "F", "Brown", "Cool", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL , 31, "M", "Brown", "Cool", 0);
